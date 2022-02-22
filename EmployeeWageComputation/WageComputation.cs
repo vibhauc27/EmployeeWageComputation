42d56ea28202dd6e3767caad404905d7555a1954
vibha <vibhauc@gmail.com>
2022-02-22 22:39:12 +0530

    [Vibha]Added:Calculating wages for a month

--- EmployeeWageComputation/WageComputation.cs
+++ EmployeeWageComputation/WageComputation.cs
@@ -7,24 +7,29 @@ namespace EmployeeWageComputation
 {
     class WageComputation
     {
-        const int WAGE_PER_HR = 20, FULL_DAY_HR = 8, FULL_TIME = 0, PART_TIME = 1, PART_TIME_HR = 4;
-        int empHrs = 0, totalEmpWage = 0;
+        const int TOTAL_WORKING_DAYS = 20, WAGE_PER_HR = 20, FULL_DAY_HR = 8, FULL_TIME = 0, PART_TIME = 1, PART_TIME_HR = 4;
+        int empHrs = 0, totalEmpWage = 0, dailyEmpWage = 0;
         public void CalculateEmpWage()
         {
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            for (int i = 0; i < TOTAL_WORKING_DAYS; i++)
             {
-                case FULL_TIME:
-                    empHrs = FULL_DAY_HR;
-                    break;
-                case PART_TIME:
-                    empHrs = PART_TIME_HR;
-                    break;
-                default:
-                    Console.WriteLine("Employee is absent");
-                    empHrs = 0;
-                    break;
+
+
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case FULL_TIME:
+                        empHrs += FULL_DAY_HR;
+                        break;
+                    case PART_TIME:
+                        empHrs += PART_TIME_HR;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+
             }
             this.totalEmpWage = empHrs * WAGE_PER_HR;
             Console.WriteLine("Total Employee wage : " + this.totalEmpWage);
