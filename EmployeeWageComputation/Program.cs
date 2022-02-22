219f69a41aed6e83d1ddacda7376968c8a634efc
vibha <vibhauc@gmail.com>
2022-02-22 21:44:29 +0530

    [Vibha]Added:To check employee is present or absent

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EmployeeWageComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WageComputation wagecomputation = new WageComputation();
+            wagecomputation.EmpAttendace();            
+        }
+    }
+}
