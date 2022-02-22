85ea46766f6bf03c0318994892de4a1aaafb0f4d
vibha <vibhauc@gmail.com>
2022-02-22 22:52:51 +0530

    [Vibha]Added:Refactor the code to write a class method to compute employee wage

--- EmployeeWageComputation/WageComputation.cs
+++ EmployeeWageComputation/WageComputation.cs
@@ -8,7 +8,7 @@ namespace EmployeeWageComputation
     class WageComputation
     {
         const int TOTAL_WORKING_HRS = 100, TOTAL_WORKING_DAYS = 20, WAGE_PER_HR = 20, FULL_DAY_HR = 8, FULL_TIME = 0, PART_TIME = 1, PART_TIME_HR = 4;
-        int empHrs = 0, totalEmpWage = 0, dailyEmpWage = 0;
+        int empHrs = 0, totalEmpWage = 0, totalEmpHrs = 0;
         public void CalculateEmpWage()
         {
             for (int i = 0; i < TOTAL_WORKING_DAYS && this.empHrs <= TOTAL_WORKING_HRS; i++)
@@ -18,18 +18,20 @@ namespace EmployeeWageComputation
                 switch (empCheck)
                 {
                     case FULL_TIME:
-                        empHrs += FULL_DAY_HR;
+                        empHrs = FULL_DAY_HR;
                         break;
                     case PART_TIME:
-                        empHrs += PART_TIME_HR;
+                        empHrs = PART_TIME_HR;
                         break;
                     default:
                         empHrs = 0;
                         break;
                 }
+                totalEmpHrs += empHrs;
 
             }
-            this.totalEmpWage = empHrs * WAGE_PER_HR;
+
+            this.totalEmpWage = totalEmpHrs * WAGE_PER_HR;
             Console.WriteLine("Total Employee wage : " + this.totalEmpWage);
         }
     }
