83ff62a243655bab8bfbd3206e1e4a0c9c10bb7f
vibha <vibhauc@gmail.com>
2022-02-22 22:46:46 +0530

    [Vibha]Added:Calculate wages till a condition of total working hours or day is reached for a month

--- EmployeeWageComputation/WageComputation.cs
+++ EmployeeWageComputation/WageComputation.cs
@@ -7,14 +7,12 @@ namespace EmployeeWageComputation
 {
     class WageComputation
     {
-        const int TOTAL_WORKING_DAYS = 20, WAGE_PER_HR = 20, FULL_DAY_HR = 8, FULL_TIME = 0, PART_TIME = 1, PART_TIME_HR = 4;
+        const int TOTAL_WORKING_HRS = 100, TOTAL_WORKING_DAYS = 20, WAGE_PER_HR = 20, FULL_DAY_HR = 8, FULL_TIME = 0, PART_TIME = 1, PART_TIME_HR = 4;
         int empHrs = 0, totalEmpWage = 0, dailyEmpWage = 0;
         public void CalculateEmpWage()
         {
-            for (int i = 0; i < TOTAL_WORKING_DAYS; i++)
+            for (int i = 0; i < TOTAL_WORKING_DAYS && this.empHrs <= TOTAL_WORKING_HRS; i++)
             {
-
-
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
                 switch (empCheck)
