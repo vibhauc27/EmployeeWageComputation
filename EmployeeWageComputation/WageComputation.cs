d6fc5428defc29f979ad3f62f103f5ab7865d5f2
vibha <vibhauc@gmail.com>
2022-02-22 22:35:45 +0530

    [Vibha]Added:Add Part time employee and wage using switch case

--- EmployeeWageComputation/WageComputation.cs
+++ EmployeeWageComputation/WageComputation.cs
@@ -7,31 +7,27 @@ namespace EmployeeWageComputation
 {
     class WageComputation
     {
-        const int WAGE_PER_HR = 20, FULL_DAY_HR = 8, FULL_TIME = 0, PART_TIME = 1, EMP_ABSENT = 2, PART_TIME_HR = 4;
+        const int WAGE_PER_HR = 20, FULL_DAY_HR = 8, FULL_TIME = 0, PART_TIME = 1, PART_TIME_HR = 4;
         int empHrs = 0, totalEmpWage = 0;
         public void CalculateEmpWage()
         {
             Random random = new Random();
             int empCheck = random.Next(0, 3);
-            if (empCheck == FULL_TIME)
+            switch (empCheck)
             {
-                empHrs = FULL_DAY_HR;
-                this.totalEmpWage = empHrs * WAGE_PER_HR;
-                Console.WriteLine("Total FullTime Employee wage : " + this.totalEmpWage);
+                case FULL_TIME:
+                    empHrs = FULL_DAY_HR;
+                    break;
+                case PART_TIME:
+                    empHrs = PART_TIME_HR;
+                    break;
+                default:
+                    Console.WriteLine("Employee is absent");
+                    empHrs = 0;
+                    break;
             }
-            if (empCheck == PART_TIME)
-            {
-                empHrs = PART_TIME_HR;
-                this.totalEmpWage = empHrs * WAGE_PER_HR;
-                Console.WriteLine("Total PartTime Employee wage : " + this.totalEmpWage);
-            }
-            if (empCheck == EMP_ABSENT)
-            {
-                empHrs = 0;
-                Console.WriteLine("Employee is absent");
-                this.totalEmpWage = empHrs * WAGE_PER_HR;
-                Console.WriteLine("Total Employee wage : " + this.totalEmpWage);
-            }
+            this.totalEmpWage = empHrs * WAGE_PER_HR;
+            Console.WriteLine("Total Employee wage : " + this.totalEmpWage);
         }
     }
 }
